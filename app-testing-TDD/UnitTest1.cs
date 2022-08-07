22bce83a255658d9444f52e2e6c23df1e12b994a
Celeste <celeste-quinn@live.nmit.ac.nz>
2022-08-07 16:35:33 +1200

    Add project files.

---
+++ app-testing-TDD/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace app_testing_TDD
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
